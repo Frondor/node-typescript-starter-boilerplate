a300d82406e9f09efb651ceb3a9446076781b549
Federico Vázquez <federico.vazquez@mercadolibre.com>
2020-04-20 20:05:34 -0300

    start develop

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express = require('express');
 const app = express();
 
 app.get('/', (req: express.Request, res: express.Response) => {
-  res.send('Hello World!');
+  res.send('Chau World!');
 });
 
 export default app;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { HTTP_PORT, HTTP_HOSTNAME } from './env';
 import server from './server';
 
-const myStr: string = 2;
-
 server.listen(HTTP_PORT, HTTP_HOSTNAME, () =>
-  console.log(`Example app listening at http://${HTTP_HOSTNAME}:${HTTP_PORT}${myStr}`)
+  console.log(`Example app listening at http://${HTTP_HOSTNAME}:${HTTP_PORT}`)
 );
